2354b99ef4add80dff62c611eb883296783f80f2
Lubova Tetrjakova <l.tetrjakova@intrum.com>
2019-03-10 14:17:40 +0200

    removed unnecessary things

--- ChessGameByLu/Program.cs
+++ ChessGameByLu/Program.cs
@@ -1,48 +1,13 @@
 ﻿using ChessGameByLu.Pieces;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace ChessGameByLu
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            //string player1;
-            //string player2;
-            //string player1Color;
-            //string player2Color;
-            //string[] colors = new string[2] { "black", "white" };
-
-            //Console.WriteLine("Name of Player 1 : ");
-            //player1 = Console.ReadLine();
-            //Console.WriteLine("Name of Player 2 : ");
-            //player2 = Console.ReadLine();
-            //Console.WriteLine("Color for Player 1 : ");
-            //player1Color = Console.ReadLine();
-            //while (!colors.Contains(player1Color.ToLower()))
-            //{
-            //    Console.WriteLine("Color was invalid, please choose black or white : ");
-            //    player1Color = Console.ReadLine().ToLower();
-            //}
-
-            //if (player1Color.ToLower() == "black")
-            //{
-            //    player2Color = "white";
-            //}
-            //else if (player1Color.ToLower() == "white")
-            //{
-            //    player2Color = "black";
-            //}
-            //else
-            //{
-            //    throw new ArgumentException("Something went wrong, check your code");
-            //}
-
-            //Console.WriteLine($"Color for {player1} : {player1Color}");
-
-            //Console.WriteLine($"Color for {player2} : {player2Color}");
 
             Board.Initiate(); // makes the board with all empty cells
 
